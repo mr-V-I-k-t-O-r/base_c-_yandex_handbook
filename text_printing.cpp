5935127c7862e05517822c1918a087d3452d9859
Viktor Goncharov <viktorgoncarov4554@gmail.com>
2025-02-02 19:41:03 +0500

    add 1 task

---
+++ text_printing.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string cpp = "C++ is a general-purpose programming language with a bias towards systems programming that\n  - is a better C\n  - supports data abstraction\n  - supports object-oriented programming\n  - supports generic programming.\n";
+    std::cout << cpp;
+    return 0;
+}
