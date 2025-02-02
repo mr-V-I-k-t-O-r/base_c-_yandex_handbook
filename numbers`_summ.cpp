234d714c24787358fe711664f1a3caf89382d289
Viktor Goncharov <viktorgoncarov4554@gmail.com>
2025-02-02 20:02:32 +0500

    add 2 task

---
+++ numbers`_summ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int first = 0;
+    int second = 0;
+
+    int result = 0;
+
+    std::cin >> first >> second;
+
+    result = first + second;
+    std::cout << result << '\n';
+    return 0;
+}
